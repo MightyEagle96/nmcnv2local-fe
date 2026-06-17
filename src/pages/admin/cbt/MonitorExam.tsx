035d10242fe175faa4ec10402e78b2382a2541ca
MightyEagle <mightyeaglecorp@gmail.com>
2026-06-17 13:33:03 +0100

    made changes

--- src/pages/admin/cbt/MonitorExam.tsx
+++ src/pages/admin/cbt/MonitorExam.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { toastError } from "../../../components/ErrorToast";
 import { httpService } from "../../../httpService";
 import { Typography } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, renderActionsCell } from "@mui/x-data-grid";
+import { Clear, Done } from "@mui/icons-material";
 
 interface Examination {
   _id: string;
@@ -43,6 +44,12 @@ function MonitorExam() {
   );
 
   const [candidates, setCandidates] = useState([]);
+  const [candidatesCount, setCandidatesCount] = useState(0);
+
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 50,
+  });
   const getActiveSession = async () => {
     try {
       const { data } = await httpService.get("examination/activesession");
@@ -54,11 +61,108 @@ function MonitorExam() {
     }
   };
 
+  const getCandidates = async () => {
+    try {
+      const { data } = await httpService.get(
+        "examination/examinationsessioncandidates",
+        {
+          params: {
+            page: paginationModel.page + 1,
+            pageSize: paginationModel.pageSize,
+          },
+        },
+      );
+
+      setCandidates(data.candidates);
+      setCandidatesCount(data.candidatesCount);
+      console.log(data);
+    } catch (error) {}
+  };
+
   useEffect(() => {
     getActiveSession();
+
+    getCandidates();
   }, []);
 
-  const columns = [{ field: "id", headerName: "ID", width: 100 }];
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    {
+      field: "indexNumber",
+      headerName: "EXAMINATION NUMBER",
+      width: 200,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">{params.row.indexNumber}</span>
+      ),
+    },
+    {
+      field: "name",
+      headerName: "NAME",
+      width: 300,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">
+          {params.row.firstName} {params.row.lastName}
+        </span>
+      ),
+    },
+    {
+      field: "ipAddress",
+      headerName: "IP ADDRESS",
+      width: 150,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">
+          {params.row.ipAddress ? params.row.ipAddress : "-"}
+        </span>
+      ),
+    },
+    {
+      field: "loginCount",
+      headerName: "LOGIN COUNT",
+      width: 150,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">
+          {params.row.loginCount ? params.row.loginCount : "-"}
+        </span>
+      ),
+    },
+    {
+      field: "loggedInTime",
+      headerName: "TIME STARTED",
+      width: 150,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">
+          {params.row.loggedInTime ? params.row.loggedInTime : "-"}
+        </span>
+      ),
+    },
+    {
+      field: "submittedTime",
+      headerName: "TIME ENDED",
+      width: 150,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">
+          {params.row.loggedInTime ? params.row.loggedInTime : "-"}
+        </span>
+      ),
+    },
+    {
+      field: "submitted",
+      headerName: "SUBMITTED",
+      width: 150,
+      renderCell: (params: any) => (
+        <span className="text-uppercase">
+          {params.row.submitted ? (
+            <Done sx={{ color: "green" }} />
+          ) : (
+            <Clear sx={{ color: "red" }} />
+          )}
+        </span>
+        // <span className="text-uppercase">
+        //   {params.row.loggedInTime ? params.row.loggedInTime : "-"}
+        // </span>
+      ),
+    },
+  ];
 
   return (
     <div className="p-3">
@@ -76,8 +180,18 @@ function MonitorExam() {
               {examinationSession.sessionName})
             </Typography>
           </div>
-          <div className=" my-3">
-            <DataGrid rows={candidates} columns={columns} />
+          <div
+            className=" my-3"
+            style={{ height: "70vh", overflowY: "scroll" }}
+          >
+            <DataGrid
+              rows={candidates}
+              columns={columns}
+              paginationMode="server"
+              paginationModel={paginationModel}
+              onPaginationModelChange={setPaginationModel}
+              rowCount={candidatesCount}
+            />
           </div>
         </>
       ) : (
